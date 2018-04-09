591c8e7da38836a08e086c29d7a69ed6727ec9b1
Jacques <jacques.leroux3@gmail.com>
2018-04-09 10:35:47 +0200

    added simplepages controller and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'simple_pages/about'
+
+  get 'simple_pages/gigs'
+
+  get 'simple_pages/music'
+
+  get 'simple_pages/shop'
+
+  get 'simple_pages/contact'
+
+  get 'simple_pages/index'
+
+  root 'simple_pages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
